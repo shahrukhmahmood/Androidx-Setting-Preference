032659943d51c895d2583443f50cb331716031ab
shahrukhmahmood <59915344+shahrukhmahmood@users.noreply.github.com>
2020-03-18 21:58:57 +0500

    update activity further implementation

--- app/src/main/java/com/shahrukhmahmood/android_setting_preference/UPDATEFragment.java
+++ app/src/main/java/com/shahrukhmahmood/android_setting_preference/UPDATEFragment.java
@@ -2,45 +2,174 @@
 package com.shahrukhmahmood.android_setting_preference;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class UPDATEFragment extends PreferenceFragmentCompat {
+
+
+    private String firstnameEdit_Value="";
+    private String lastnameEdit_Value="";
+    SharedPreferences sharedPreferences;
+   // EditTextPreference editFirstName1= findPreference("firstName");
+
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.update_account_setting_preference);
 
-        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE);
+         sharedPreferences = this.getActivity().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE);
 
         try{
-            EditTextPreference editFirstName = findPreference("firstName");
-            EditTextPreference editLastName = findPreference("lastName");
-            EditTextPreference editEmail = findPreference("email");
-            EditTextPreference editPhoneNumber = findPreference("phoneNumber");
+            final EditTextPreference editTextFirstName = findPreference("firstName");
+            final EditTextPreference editTextLastName = findPreference("lastName");
+            final EditTextPreference editTextEmail = findPreference("email");
+            final EditTextPreference editTextPhoneNumber = findPreference("phoneNumber");
+            final EditTextPreference editTextPassword = findPreference("password");
 
-            editFirstName.setSummary(sharedPreferences.getString("first_name", ""));
-            editFirstName.setText(sharedPreferences.getString("first_name", ""));
+            editTextFirstName.setSummary(sharedPreferences.getString("first_name", ""));
+            editTextFirstName.setText(sharedPreferences.getString("first_name", ""));
+            editTextFirstName.setIconSpaceReserved(false);
 
-            editLastName.setSummary(sharedPreferences.getString("last_name", ""));
-            editLastName.setText(sharedPreferences.getString("last_name", ""));
+            editTextLastName.setSummary(sharedPreferences.getString("last_name", ""));
+            editTextLastName.setText(sharedPreferences.getString("last_name", ""));
+            editTextLastName.setIconSpaceReserved(false);
 
-            editEmail.setSummary(sharedPreferences.getString("email", ""));
-            editEmail.setText(sharedPreferences.getString("email", ""));
+            editTextEmail.setSummary(sharedPreferences.getString("email", ""));
+            editTextEmail.setText(sharedPreferences.getString("email", ""));
+            editTextEmail.setIconSpaceReserved(false);
 
-            editPhoneNumber.setSummary(sharedPreferences.getString("phone_number", ""));
-            editPhoneNumber.setText(sharedPreferences.getString("phone_number", ""));
+            editTextPhoneNumber.setSummary(sharedPreferences.getString("phone_number", ""));
+            editTextPhoneNumber.setText(sharedPreferences.getString("phone_number", ""));
+            editTextPhoneNumber.setIconSpaceReserved(false);
+
+            editTextPassword.setSummary("**********");
+            editTextPassword.setIconSpaceReserved(false);
+
+
+            editTextFirstName.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                    firstnameEdit_Value = newValue.toString();
+                    name_update();
+                    editTextFirstName.setSummary(firstnameEdit_Value);
+                    return true;
+
+                }
+            });
+
+
+            editTextLastName.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                    lastnameEdit_Value = newValue.toString();
+                    name_update();
+                    editTextLastName.setSummary(lastnameEdit_Value);
+                    return true;
+
+                }
+            });
+
         }
         catch (Exception e){
             e.printStackTrace();
         }
 
     }
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+
+    }
+
+
+    public void name_update() {
+
+        String url = "http://52.15.104.184/update/name/";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    //@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            if (json.getString("status").equals("200")) {
+
+
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                // editor.putString("Token", token);
+                                //editor.apply();
+                                editor.putString("first_name", firstnameEdit_Value);
+
+                                editor.apply();
+                                Toast.makeText(getActivity(), json.getString("message"), Toast.LENGTH_LONG).show();
+
+                                //Intent i = new Intent(Login.this, SettingsActivity.class);
+                                //Login.this.startActivity(i);
+
+                            }
+
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                })
+
+
+        {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("first_name",firstnameEdit_Value);
+                params.put("last_name",lastnameEdit_Value);
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", sharedPreferences.getString("Token", ""));
+                return headers;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
 
     }
 
