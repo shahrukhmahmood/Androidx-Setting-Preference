3365d925442967801a9aa6f27846134f40f24d97
shahrukhmahmood <59915344+shahrukhmahmood@users.noreply.github.com>
2020-03-21 04:35:29 +0500

    verify and update password done

--- app/src/main/java/com/shahrukhmahmood/android_setting_preference/UpdateFragment.java
+++ app/src/main/java/com/shahrukhmahmood/android_setting_preference/UpdateFragment.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.preference.DialogPreference;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -34,6 +36,7 @@ public class UpdateFragment extends PreferenceFragmentCompat {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.update_account_setting_preference);
+        final PreferenceScreen preferenceScreen = this.getPreferenceScreen();
 
         sharedPreferences = this.getActivity().getSharedPreferences("AppPreferences", Context.MODE_PRIVATE);
         try{
@@ -41,7 +44,10 @@ public class UpdateFragment extends PreferenceFragmentCompat {
             final EditTextPreference editTextLastName = findPreference("lastName");
             final EditTextPreference editTextEmail = findPreference("email");
             final EditTextPreference editTextPhoneNumber = findPreference("phoneNumber");
-            final EditTextPreference editTextPassword = findPreference("password");
+          //  final EditTextPreference editTextPassword = findPreference("password");
+            final  Preference preferencePassword = findPreference("password");
+
+
 
             editTextFirstName.setSummary(sharedPreferences.getString("first_name", ""));
 //            editTextFirstName.setText(sharedPreferences.getString("first_name", ""));
@@ -59,8 +65,10 @@ public class UpdateFragment extends PreferenceFragmentCompat {
 //            editTextPhoneNumber.setText(sharedPreferences.getString("phone_number", ""));
             editTextPhoneNumber.setIconSpaceReserved(false);
 
-            editTextPassword.setSummary("********");
-            editTextPassword.setIconSpaceReserved(false);
+            //editTextPassword.setSummary("********");
+            preferencePassword.setIconSpaceReserved(false);
+           // editTextPassword.setIconSpaceReserved(false);
+
 
 
             editTextFirstName.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
@@ -92,6 +100,7 @@ public class UpdateFragment extends PreferenceFragmentCompat {
                 }
             });
 
+
             editTextPhoneNumber.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
@@ -99,6 +108,18 @@ public class UpdateFragment extends PreferenceFragmentCompat {
 //                    editTextPhoneNumber.setIntent(new Intent(getContext(), Login.class));
                     Intent i = new Intent(getContext(), Login.class);
                     startActivity(i);
+                    return true;
+                }
+            });
+
+
+
+
+            preferencePassword.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent i = new Intent(getActivity(), Verifypassword.class);
+                    UpdateFragment.this.startActivity(i);
+
                     return true;
                 }
             });
@@ -184,113 +205,5 @@ public class UpdateFragment extends PreferenceFragmentCompat {
             requestQueue.add(stringRequest);
             return flag;
     }
-
-//    private SharedPreferences sharedPreferences;
-//    private String firstName;
-//    private String lastName;
-//    private String phoneNumber;
-//    private String email;
-//    private String publicFirstName;
-//    private String publicLastName;
-//    private String publicphone;
-//    private String publicemail;
-//    public  String token1;
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-////        getSupportActionBar().hide();
-//
-//        try{
-//            token1 = Login.token;
-//
-//            addPreferencesFromResource(R.xml.AccountSettingPreference);
-//            account_details();
-//
-//            final PreferenceCategory preferenceCategory = (PreferenceCategory) findPreference("name_cat");
-//            PreferenceScreen preferenceScreen = this.getPreferenceScreen();
-//
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    @Override
-//    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-//        /*setPreferencesFromResource(R.xml.AccountSettingPreference, rootKey);*/
-//    }
-//
-//    public void account_details()
-//    {
-//        String url = "http://52.15.104.184/my_details/";
-//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//
-//                        try {
-//                            JSONObject jsonObject = new JSONObject(response);
-//                            if (jsonObject.getString("status").equals("200")) {
-//
-//                                publicFirstName = jsonObject.getString("first_name");
-//                                publicLastName = jsonObject.getString("last_name");
-//                                publicphone = jsonObject.getString("phone_number");
-//                                publicemail = jsonObject.getString("email");
-//
-//                                publicFirstName= publicFirstName+" "+publicLastName;
-//                                Preference mPref2 = findPreference("name");
-//                                mPref2.setTitle(publicFirstName);
-//                                mPref2.setTitle(publicemail);
-//                                mPref2.setSummary(publicphone);
-//                                Log.e("VOLLEY", publicFirstName);
-//
-//                                Toast.makeText(getActivity(), response, Toast.LENGTH_LONG).show();
-//                            }
-//                        }
-//                        catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                },
-//
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(getActivity(),error.toString(),Toast.LENGTH_LONG).show();
-//                    }
-//                }){
-//            @Override
-//            protected Map<String,String> getParams(){
-//                Map<String,String> params = new HashMap<>();
-//
-//                return params;
-//            }
-//
-//            @Override
-//            public Map<String, String> getHeaders() throws AuthFailureError {
-//                Map<String, String> headers = new HashMap<String, String>();
-//                headers.put("Authorization", token1);
-//                return headers;
-//            }
-//
-//        };
-//
-//        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
-//        requestQueue.add(stringRequest);
-//
-//
-//
-//
-//
-//
-//        //  PreferenceCategory preferenceCategory = new PreferenceCategory(preferenceScreen.getContext());
-//        // preferenceCategory.setTitle("Wireless switches");
-//        // Preference mPref2 = findPreference("name");
-//        //mPref2.setTitle(publicphone);
-//        //  preferenceScreen.addPreference(preferenceCategory);
-//
-//    }
 
 }
---
+++ app/src/main/java/com/shahrukhmahmood/android_setting_preference/Verifypassword.java
@@ -0,0 +1,134 @@
+package com.shahrukhmahmood.android_setting_preference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.shahrukhmahmood.android_setting_preference.Login.AppPreferences;
+
+public class Verifypassword extends AppCompatActivity {
+
+    Button button_verify_pass;
+    EditText editText_password;
+    SharedPreferences sharedPreferences;
+    TextView error_message;
+    TextView password_message;
+    //Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verifypassword);
+
+        sharedPreferences = Verifypassword.this.getSharedPreferences(AppPreferences, Context.MODE_PRIVATE);
+        editText_password= findViewById(R.id.verify_password);
+        button_verify_pass= findViewById(R.id.verifypassword);
+        error_message=findViewById(R.id.errormessage);
+        password_message=findViewById(R.id.securitytext);
+
+
+        button_verify_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verify_password();
+            }
+        });
+    }
+
+
+    public void verify_password()
+    {
+        final String password_verify = editText_password.getText().toString();
+
+        if(password_verify.equals(""))
+        {
+            Toast.makeText(Verifypassword.this, "Field cannot be empty", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            try {
+
+                String url = "http://52.15.104.184/password/check/";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            //@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                            @Override
+                            public void onResponse(String response) {
+
+                                try {
+                                    JSONObject json = new JSONObject(response);
+                                    if (json.getString("status").equals("200")) {
+
+
+                                        // SharedPreferences.Editor editor = sharedPreferences.edit();
+                                        //editor.putString("Token", token);
+                                        //editor.apply();
+
+                                        Toast.makeText(Verifypassword.this, json.getString("message"), Toast.LENGTH_LONG).show();
+
+                                        Intent i = new Intent(Verifypassword.this, Updatepassword.class);
+                                        Verifypassword.this.startActivity(i);
+
+                                    } else if (json.getString("status").equals("401")) {
+
+                                         password_message.setVisibility(View.GONE);
+                                        error_message.setText(json.getString("message"));
+                                       // Toast.makeText(Verifypassword.this, json.getString("message"), Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(Verifypassword.this, error.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("password", password_verify);
+
+                        return params;
+                    }
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<String, String>();
+                        headers.put("Authorization", sharedPreferences.getString("Token", ""));
+                        return headers;
+                    }
+
+                };
+
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
